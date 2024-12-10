ba03335dcb79db973f52d0ef32a0900d9fe72227
REIARTHUR <reiarthur635@gmail.com>
2024-12-10 20:56:06 -0300

    Projeto incompleto

--- app.py
+++ app.py
@@ -1,92 +1,103 @@
 from flask import Flask, render_template, request, redirect, url_for
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-# Dados em memória
-usuarios = []
-livros = []
-locacoes = []
+# Dados simulados em memória
+alunos = []  # Lista para armazenar alunos
+livros = []  # Lista para armazenar livros
+emprestimos = []  # Lista para armazenar empréstimos
 
+# Página inicial
 @app.route('/')
 def index():
-    """Página inicial"""
-    for locacao in locacoes:
-        if locacao['data_devolucao']:
-            locacao['atrasado'] = False
-        else:
-            data_prevista = datetime.strptime(locacao['data_prevista'], '%Y-%m-%d')
-            hoje = datetime.now()
-            locacao['atrasado'] = hoje > data_prevista
-    return render_template('index.html', locacoes=locacoes)
+    return render_template('index.html')
 
-# Páginas de gerenciamento
-@app.route('/usuarios')
-def gerenciar_usuarios():
-    """Página para gerenciar usuários"""
-    return render_template('usuarios.html', usuarios=usuarios)
-
-@app.route('/livros')
-def gerenciar_livros():
-    """Página para gerenciar livros"""
-    return render_template('livros.html', livros=livros)
-
-# Cadastro de usuários
-@app.route('/cadastrar_usuario', methods=['POST'])
-def cadastrar_usuario():
-    id_usuario = len(usuarios) + 1
-    nome = request.form['nome']
-    turma = request.form['turma']
-    nome_turma = request.form['nome_turma']
-    usuarios.append({'id': id_usuario, 'nome': nome, 'turma': turma, 'nome_turma': nome_turma})
-    return redirect(url_for('gerenciar_usuarios'))
+# Cadastro de alunos
+@app.route('/cadastrar_aluno', methods=['GET', 'POST'])
+def cadastrar_aluno():
+    if request.method == 'POST':
+        aluno = {
+            'id': int(request.form['id']),
+            'nome': request.form['nome'],
+            'turma': request.form['turma'],
+            'serie': request.form['serie'],
+            'turno': request.form['turno']
+        }
+        alunos.append(aluno)
+        return redirect(url_for('cadastrar_aluno'))
+    return render_template('cadastrar_aluno.html', alunos=alunos)
 
 # Cadastro de livros
-@app.route('/cadastrar_livro', methods=['POST'])
+@app.route('/cadastrar_livro', methods=['GET', 'POST'])
 def cadastrar_livro():
-    id_livro = len(livros) + 1
-    titulo = request.form['titulo']
-    autor = request.form['autor']
-    ano = request.form['ano']
-    livros.append({'id': id_livro, 'titulo': titulo, 'autor': autor, 'ano': ano})
-    return redirect(url_for('gerenciar_livros'))
+    if request.method == 'POST':
+        livro = {
+            'id': int(request.form['id']),
+            'titulo': request.form['titulo'],
+            'autor': request.form['autor'],
+            'ano': int(request.form['ano'])
+        }
+        livros.append(livro)
+        return redirect(url_for('cadastrar_livro'))
+    return render_template('cadastrar_livro.html', livros=livros)
 
-# Registro de locações
-@app.route('/registrar_locacao', methods=['POST'])
-def registrar_locacao():
-    id_usuario = int(request.form['id_usuario'])
-    id_livro = int(request.form['id_livro'])
-    data_retirada = request.form['data_retirada']
-    data_prevista = request.form['data_prevista']
-    locacoes.append({
-        'id': len(locacoes) + 1,
-        'id_usuario': id_usuario,
-        'id_livro': id_livro,
-        'data_retirada': data_retirada,
-        'data_prevista': data_prevista,
-        'data_devolucao': None,
-        'atrasado': False
-    })
-    return redirect(url_for('index'))
+# Registro de empréstimos
+@app.route('/registrar_emprestimo', methods=['GET', 'POST'])
+def registrar_emprestimo():
+    if request.method == 'POST':
+        emprestimo = {
+            'aluno_id': int(request.form['aluno_id']),
+            'livro_id': int(request.form['livro_id']),
+            'data_retirada': datetime.now().strftime('%Y-%m-%d'),
+            'data_prevista': (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d'),
+            'devolvido': False
+        }
+        emprestimos.append(emprestimo)
+        return redirect(url_for('registrar_emprestimo'))
+    return render_template('registrar_emprestimo.html', alunos=alunos, livros=livros, emprestimos=emprestimos)
 
-# Registro de devolução
-@app.route('/registrar_devolucao/<int:locacao_id>', methods=['POST'])
-def registrar_devolucao(locacao_id):
-    data_devolucao = request.form['data_devolucao']
-    for locacao in locacoes:
-        if locacao['id'] == locacao_id:
-            locacao['data_devolucao'] = data_devolucao
-            locacao['atrasado'] = False
-            break
-    return redirect(url_for('index'))
+# Registro de devoluções
+@app.route('/registrar_devolucao', methods=['GET', 'POST'])
+def registrar_devolucao():
+    if request.method == 'POST':
+        aluno_id = int(request.form['aluno_id'])
+        livro_id = int(request.form['livro_id'])
+        for emprestimo in emprestimos:
+            if emprestimo['aluno_id'] == aluno_id and emprestimo['livro_id'] == livro_id:
+                emprestimo['devolvido'] = True
+                emprestimo['data_devolucao'] = datetime.now().strftime('%Y-%m-%d')
+                break
+        return redirect(url_for('registrar_devolucao'))
+    return render_template('registrar_devolucao.html', emprestimos=emprestimos, alunos=alunos, livros=livros)
 
-# Busca por usuários e livros
-@app.route('/buscar', methods=['GET'])
-def buscar():
-    termo = request.args.get('termo', '').lower()
-    resultados_usuarios = [usuario for usuario in usuarios if termo in usuario['nome'].lower()]
-    resultados_livros = [livro for livro in livros if termo in livro['titulo'].lower()]
-    return render_template('busca.html', termo=termo, usuarios=resultados_usuarios, livros=resultados_livros)
+# Lista de atrasados
+@app.route('/atrasados')
+def atrasados():
+    atrasados_list = []
+    for emprestimo in emprestimos:
+        if not emprestimo['devolvido'] and datetime.strptime(emprestimo['data_prevista'], '%Y-%m-%d') < datetime.now():
+            aluno = next((al for al in alunos if al['id'] == emprestimo['aluno_id']), None)
+            livro = next((lv for lv in livros if lv['id'] == emprestimo['livro_id']), None)
+            atrasados_list.append({
+                'aluno': aluno,
+                'livro': livro,
+                'data_prevista': emprestimo['data_prevista']
+            })
+    return render_template('atrasados.html', atrasados=atrasados_list)
+
+# Histórico de devoluções
+@app.route('/historico')
+def historico():
+    historico_list = [
+        {
+            'aluno': next((al for al in alunos if al['id'] == emp['aluno_id']), None),
+            'livro': next((lv for lv in livros if lv['id'] == emp['livro_id']), None),
+            'data_devolucao': emp.get('data_devolucao')
+        }
+        for emp in emprestimos if emp['devolvido']
+    ]
+    return render_template('historico.html', historico=historico_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
