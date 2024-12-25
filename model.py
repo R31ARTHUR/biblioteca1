dc7ed1c4a8d33add4ea75155ad68bde64e67ca0f
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-12-25 08:34:35 -0300

    implementando modelo e orm sqlalchemy

--- model.py
+++ model.py
@@ -1,111 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
 
-class Livro:
-    """
-        Uma classe Livro com atributos: id, titulo, autor, categoria, editora, edicao, ano, indicacao
-    """
-    def __init__(
-            self,
-            id,
-            titulo,
-            categoria,
-            editora,
-            edicao,
-            ano,
-            indicacao):
-        self._id = id
-        self._titulo = titulo
-        self._categoria = categoria
-        self._editora = editora
-        self._edicao = edicao
-        self._ano = ano
-        self._indicacao = indicacao
+db = SQLAlchemy()
+
+class Livro(db.Model):
+    __tablename__ = 'livros'
     
-    @property
-    def id(self):
-        return self._id
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(150), nullable=False)
+    categoria = db.Column(db.String(100))
+    editora = db.Column(db.String(100))
+    edicao = db.Column(db.String(50))
+    ano = db.Column(db.Integer)
+    indicacao = db.Column(db.String(150))
 
-    @id.setter
-    def id(self, id):
-        self._id = id
+    def __repr__(self):
+        return f"<Livro {self.titulo}>"
 
-    @property
-    def titulo(self):
-        return self._titulo
-
-    @titulo.setter
-    def titulo(self, titulo):
-        self._titulo = titulo
-
-    @property
-    def categoria(self):
-        return self._categoria
-
-    @categoria.setter
-    def categoria(self, categoria):
-        self._categoria = categoria
-
-    @property
-    def editora(self):
-        return self._editora
-
-    @editora.setter
-    def editora(self, editora):
-        self._editora = editora
-
-    @property
-    def edicao(self):
-        return self._edicao
-
-    @edicao.setter
-    def edicao(self, edicao):
-        self._edicao = edicao
-
-    @property
-    def ano(self):
-        return self._ano
-
-    @ano.setter
-    def ano(self, ano):
-        self._ano = ano
-
-    @property
-    def indicacao(self):
-        return self._indicacao
-
-    @indicacao.setter
-    def indicacao(self, indicacao):
-        self._indicacao = indicacao
-
-    def __str__(self):
-        return f"ID: {self._id} | Título: {self._titulo} | Categoria: {self._categoria} | Editora: {self._editora} | Edição: {self._edicao} | Ano: {self._ano} | Indicação: {self._indicacao}"
-
-class Aluno:
-    """
-        Uma classe Aluno com atributos: matricula, nome
-    """
-    def __init__(
-            self,
-            matricula,
-            nome,):
-        self._matricula = matricula
-        self._nome = nome
+class Aluno(db.Model):
+    __tablename__ = 'alunos'
     
-    @property
-    def matricula(self):
-        return self._matricula
+    matricula = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
 
-    @matricula.setter
-    def matricula(self, matricula):
-        self._matricula = matricula
+    def __repr__(self):
+        return f"<Aluno {self.nome}>"
 
-    @property
-    def nome(self):
-        return self._nome
-
-    @nome.setter
-    def nome(self, nome):
-        self._nome = nome
-
+class Emprestimo(db.Model):
+    __tablename__ = 'emprestimos'
     
-    def __str__(self):
-        return f"ID: {self._matricula} | Nome: {self._nome}"
+    id = db.Column(db.Integer, primary_key=True)
+    aluno_id = db.Column(db.Integer, db.ForeignKey('alunos.matricula'), nullable=False)
+    livro_id = db.Column(db.Integer, db.ForeignKey('livros.id'), nullable=False)
+    data_emprestimo = db.Column(db.Date, nullable=False)
+    data_devolucao = db.Column(db.Date)
+
+    aluno = db.relationship('Aluno', backref='emprestimos')
+    livro = db.relationship('Livro', backref='emprestimos')
+
+    def __repr__(self):
+        return f"<Emprestimo Aluno: {self.aluno.nome}, Livro: {self.livro.titulo}>"
